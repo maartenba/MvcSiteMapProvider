7eaa8ba4babdfaadcca7451100be240d8539b2be
Maarten Balliauw <maarten@balliauw.be>
2012-08-22 21:55:06 +0200

    And there's a fresh build!
    And there's a fresh build!

--- build/build.ps1
+++ build/build.ps1
@@ -4,7 +4,7 @@ properties {
     $source_directory = "$base_directory\src\MvcSiteMapProvider"
     $nuget_directory  = "$base_directory\nuget"
     $tools_directory  = "$base_directory\tools"
-    $version          = "3.2.3.0"
+    $version          = "3.3.2.0"
 }
 
 include .\psake_ext.ps1
@@ -32,18 +32,18 @@ task Init -description "This tasks makes sure the build environment is correctly
     }
 }
 
-task Compile35 -depends Clean, Init -description "This task compiles the solution" {
-    exec { 
-        msbuild $source_directory\$solution.sln `
-            /p:outdir=$build_directory\\$solution\\net35\\ `
-            /verbosity:quiet `
-            /p:Configuration=Release `
-			/t:Rebuild `
-			/property:WarningLevel=3 `
-            /property:TargetFrameworkVersion=v3.5 `
-            /property:DefineConstants=NET35
-    }
-}
+# task Compile35 -depends Clean, Init -description "This task compiles the solution" {
+    # exec { 
+        # msbuild $source_directory\$solution.sln `
+            # /p:outdir=$build_directory\\$solution\\net35\\ `
+            # /verbosity:quiet `
+            # /p:Configuration=Release `
+			# /t:Rebuild `
+			# /property:WarningLevel=3 `
+            # /property:TargetFrameworkVersion=v3.5 `
+            # /property:DefineConstants=NET35
+    # }
+# }
 
 task Compile -depends Clean, Init -description "This task compiles the solution" {
     exec { 
@@ -58,13 +58,13 @@ task Compile -depends Clean, Init -description "This task compiles the solution"
     }
 }
 
-task NuGet -depends Compile35,Compile -description "This tasks makes creates the NuGet packages" {  
+task NuGet -depends Compile -description "This tasks makes creates the NuGet packages" {  
     Generate-Nuspec-File `
 		-file "$nuget_directory\mvcsitemapprovider.nuspec" `
 		-version $version
 
     Copy-Item $source_directory\MvcSiteMapProvider\MvcSiteMapSchema.xsd $nuget_directory\content
-    Copy-Item $base_directory\release\MvcSiteMapProvider\* $nuget_directory\lib -Recurse
+    Copy-Item $base_directory\release\MvcSiteMapProvider\* $nuget_directory\lib\net40 -Recurse
 
     exec { 
         &"$tools_directory\nuget\NuGet.exe" pack $nuget_directory\mvcsitemapprovider.nuspec -Symbols
--- src/MvcSiteMapProvider/MvcSiteMapProvider/Properties/AssemblyInfo.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliantAttribute(false )]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyTitleAttribute("MvcSiteMapProvider 3.2.3.0")]
+[assembly: AssemblyTitleAttribute("MvcSiteMapProvider 3.3.2.0")]
 [assembly: AssemblyDescriptionAttribute("An ASP.NET SiteMapProvider implementation for the ASP.NET MVC framework.")]
 [assembly: AssemblyCompanyAttribute("MvcSiteMapProvider")]
-[assembly: AssemblyProductAttribute("MvcSiteMapProvider 3.2.3.0")]
+[assembly: AssemblyProductAttribute("MvcSiteMapProvider 3.3.2.0")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Maarten Balliauw 2009 - 2012")]
-[assembly: AssemblyVersionAttribute("3.2.3.0")]
-[assembly: AssemblyInformationalVersionAttribute("3.2.3.0")]
-[assembly: AssemblyFileVersionAttribute("3.2.3.0")]
+[assembly: AssemblyVersionAttribute("3.3.2.0")]
+[assembly: AssemblyInformationalVersionAttribute("3.3.2.0")]
+[assembly: AssemblyFileVersionAttribute("3.3.2.0")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
