7c72c38386d073b72c38c3cf8c0ca9a495450d0c
Shad Storhaug <shad@shadstorhaug.com>
2013-01-23 19:49:30 +0700

    Added MvcSiteMapProvider.Core and MvcSiteMapProvider.Web libraries for building a new prototype.

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider.Web/MvcSiteMapProvider.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MvcSiteMapProvider.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MvcSiteMapProvider.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class MvcSiteMapProvider
+    {
+    }
+}
