5cc3aab413f3581f4312b8b83e8b4cb6431cda73
Shad Storhaug <shad@shadstorhaug.com>
2013-02-14 19:05:27 +0700

    Moved ThreadSafeDictionary to the Core.Collections namespace and moved the interface to a separate file.

--- src/MvcSiteMapProvider/MvcSiteMapProvider.Core/Mvc/DefaultActionMethodParameterResolver.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider.Core/Mvc/DefaultActionMethodParameterResolver.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Mvc;
 using System.Web.Mvc.Async;
 using MvcSiteMapProvider.Core;
+using MvcSiteMapProvider.Core.Collections;
 
 namespace MvcSiteMapProvider.Core.Mvc
 {
