c02dcd7a2e8e9f73eb7310d77693356af3371c7a
Shad Storhaug <shad@shadstorhaug.com>
2013-02-23 23:07:20 +0700

    Corrected compiler switches in IUrlHelper for MVC2.

--- src/MvcSiteMapProvider/MvcSiteMapProvider/Web/Mvc/IUrlHelper.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Web/Mvc/IUrlHelper.cs
@@ -13,23 +13,23 @@ namespace MvcSiteMapProvider.Web
         string Action(string actionName, string controllerName);
         string Action(string actionName, string controllerName, object routeValues);
         string Action(string actionName, string controllerName, object routeValues, string protocol);
+        string Action(string actionName, string controllerName, RouteValueDictionary routeValues);
+        string Action(string actionName, string controllerName, RouteValueDictionary routeValues, string protocol, string hostName);
+        string Action(string actionName, RouteValueDictionary routeValues);
         string Content(string contentPath);
         string Encode(string url);
+        RequestContext RequestContext { get; }
+        RouteCollection RouteCollection { get; }
         string RouteUrl(object routeValues);
         string RouteUrl(string routeName);
         string RouteUrl(string routeName, object routeValues);
         string RouteUrl(string routeName, object routeValues, string protocol);
-
-#if !MVC2
-        string Action(string actionName, string controllerName, RouteValueDictionary routeValues);
-        string Action(string actionName, string controllerName, RouteValueDictionary routeValues, string protocol, string hostName);
-        string Action(string actionName, RouteValueDictionary routeValues);
-        bool IsLocalUrl(string url);
-        RequestContext RequestContext { get; }
-        RouteCollection RouteCollection { get; }
         string RouteUrl(string routeName, RouteValueDictionary routeValues);
         string RouteUrl(string routeName, RouteValueDictionary routeValues, string protocol, string hostName);
         string RouteUrl(RouteValueDictionary routeValues);
+
+#if !MVC2
+        bool IsLocalUrl(string url);
 #endif
 #if MVC4
         string HttpRouteUrl(string routeName, object routeValues);
