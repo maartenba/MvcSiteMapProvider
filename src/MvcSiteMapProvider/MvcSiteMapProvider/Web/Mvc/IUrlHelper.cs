99b18659145b365a7b20f0512e9f35bf4cbb90da
Shad Storhaug <shad@shadstorhaug.com>
2013-02-23 22:02:45 +0700

    Updated IUrlHelper interface to take into consideration changes to the MVC framework in MVC2 and MVC3.

--- src/MvcSiteMapProvider/MvcSiteMapProvider/Web/Mvc/IUrlHelper.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Web/Mvc/IUrlHelper.cs
@@ -13,22 +13,28 @@ namespace MvcSiteMapProvider.Web
         string Action(string actionName, string controllerName);
         string Action(string actionName, string controllerName, object routeValues);
         string Action(string actionName, string controllerName, object routeValues, string protocol);
-        string Action(string actionName, string controllerName, RouteValueDictionary routeValues);
-        string Action(string actionName, string controllerName, RouteValueDictionary routeValues, string protocol, string hostName);
-        string Action(string actionName, RouteValueDictionary routeValues);
         string Content(string contentPath);
         string Encode(string url);
-        string HttpRouteUrl(string routeName, object routeValues);
-        string HttpRouteUrl(string routeName, RouteValueDictionary routeValues);
-        bool IsLocalUrl(string url);
-        RequestContext RequestContext { get; }
-        RouteCollection RouteCollection { get; }
         string RouteUrl(object routeValues);
         string RouteUrl(string routeName);
         string RouteUrl(string routeName, object routeValues);
         string RouteUrl(string routeName, object routeValues, string protocol);
+
+#if !MVC2
+        string Action(string actionName, string controllerName, RouteValueDictionary routeValues);
+        string Action(string actionName, string controllerName, RouteValueDictionary routeValues, string protocol, string hostName);
+        string Action(string actionName, RouteValueDictionary routeValues);
+        bool IsLocalUrl(string url);
+        RequestContext RequestContext { get; }
+        RouteCollection RouteCollection { get; }
         string RouteUrl(string routeName, RouteValueDictionary routeValues);
         string RouteUrl(string routeName, RouteValueDictionary routeValues, string protocol, string hostName);
         string RouteUrl(RouteValueDictionary routeValues);
+#endif
+#if MVC4
+        string HttpRouteUrl(string routeName, object routeValues);
+        string HttpRouteUrl(string routeName, RouteValueDictionary routeValues);
+#endif
+
     }
 }
