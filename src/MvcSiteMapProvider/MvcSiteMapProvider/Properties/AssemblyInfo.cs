08c51ec994e8590f4ef79984c13eaaa30f26491c
Shad Storhaug <shad@shadstorhaug.com>
2013-03-17 21:32:56 +0700

    Set version to 4.0.0.0 and copied over copyright and other details of the assembly.

--- src/MvcSiteMapProvider/MvcSiteMapProvider/Properties/AssemblyInfo.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Properties/AssemblyInfo.cs
@@ -1,26 +1,26 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvcSiteMapProvider")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MvcSiteMapProvider 4.0.0.0")]
+[assembly: AssemblyDescription("An ASP.NET SiteMapProvider implementation for the ASP.NET MVC framework.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvcSiteMapProvider")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("MvcSiteMapProvider")]
+[assembly: AssemblyProduct("MvcSiteMapProvider 4.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Maarten Balliauw 2009 - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyDelaySignAttribute(false)]
+[assembly: CLSCompliantAttribute(false)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6301e69b-9edb-4916-a5f5-dacea37cfbbc")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 
 #if !NET35
 // Startup using WebActivatorEx (which allows multiple things to be started up, as opposed to the System.Web version).
