777082295bf97ff1e191b62feceb85ae35531646
Shad Storhaug <shad@shadstorhaug.com>
2013-03-23 22:29:56 +0700

    Re-added ControllerExtensions class.

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Web/Mvc/ControllerExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSiteMapProvider.Web.Mvc
+{
+    /// <summary>
+    /// ControllerExtensions
+    /// </summary>
+    public static class ControllerExtensions
+    {
+        /// <summary>
+        /// Gets the current site map node.
+        /// </summary>
+        /// <param name="controller">The controller.</param>
+        /// <returns></returns>
+        public static ISiteMapNode GetCurrentSiteMapNode(this ControllerBase controller)
+        {
+            return GetCurrentSiteMapNode(controller, SiteMaps.Current);
+        }
+
+        /// <summary>
+        /// Gets the current site map node.
+        /// </summary>
+        /// <param name="controller">The controller.</param>
+        /// <param name="siteMap">The site map.</param>
+        /// <returns></returns>
+        public static ISiteMapNode GetCurrentSiteMapNode(this ControllerBase controller, ISiteMap siteMap)
+        {
+            return siteMap.CurrentNode;
+        }
+
+        /// <summary>
+        /// Gets the current site map node for child action.
+        /// </summary>
+        /// <param name="controller">The controller.</param>
+        /// <returns></returns>
+        public static ISiteMapNode GetCurrentSiteMapNodeForChildAction(this ControllerBase controller)
+        {
+            return GetCurrentSiteMapNodeForChildAction(controller, SiteMaps.Current);
+        }
+
+        /// <summary>
+        /// Gets the current site map node for child action.
+        /// </summary>
+        /// <param name="controller">The controller.</param>
+        /// <param name="provider">The provider.</param>
+        /// <returns></returns>
+        public static ISiteMapNode GetCurrentSiteMapNodeForChildAction(this ControllerBase controller, ISiteMap siteMap)
+        {
+            return siteMap.FindSiteMapNode(controller.ControllerContext);
+        }
+    }
+}
