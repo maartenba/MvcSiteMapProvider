213353c8167f41635f98e630b6da801814c6c63e
Maarten Balliauw <maarten@maartenballiauw.be>
2013-04-17 09:42:00 +0300

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -5,9 +5,10 @@ if "%config%" == "" (
    set config=Release
 )
 
-set version=
-if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
-)
+REM set version=
+REM if not "%PackageVersion%" == "" (
+REM    set version=%PackageVersion%
+REM )
+set version=4.0.0
 
 powershell -Command "& { Import-Module .\build\psake.psm1; $psake.use_exit_on_error = $true; Invoke-psake .\build\build.ps1 -framework 4.0x64 -properties @{\"version\"=\"%version%\";\"configuration\"=\"%config%"\";\"packageVersion\"=\"%PackageVersion%\"} }"
