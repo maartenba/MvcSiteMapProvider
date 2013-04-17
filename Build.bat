d854a2cd70c985bd6c711118b8f24522d71c72a6
Shad Storhaug <shad@shadstorhaug.com>
2013-04-17 21:02:36 +0700

    Merged changes with v4 branch.

--- Build.bat
+++ Build.bat
@@ -1,1 +1,14 @@
-powershell -Command "& { Import-Module .\build\psake.psm1; $psake.use_exit_on_error = $true; Invoke-psake .\build\build.ps1 -framework 4.0x64 }"
+@echo off
+
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+REM set version=
+REM if not "%PackageVersion%" == "" (
+REM    set version=%PackageVersion%
+REM )
+set version=4.0.0
+
+powershell -Command "& { Import-Module .\build\psake.psm1; $psake.use_exit_on_error = $true; Invoke-psake .\build\build.ps1 -framework 4.0x64 -properties @{\"version\"=\"%version%\";\"configuration\"=\"%config%"\";\"packageVersion\"=\"%PackageVersion%\"} }"
--- BuildTemp.bat
+++
@@ -1,4 +0,0 @@
-set nuget=.\src\MvcSiteMapProvider\.nuget\NuGet.exe
-
-mkdir packages
-%nuget% pack .\src\MvcSiteMapProvider\MvcSiteMapProvider\MvcSiteMapProvider.csproj -NonInteractive -Build -Symbols -OutputDirectory .\packages
