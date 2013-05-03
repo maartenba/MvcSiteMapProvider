df1fec533a7cf5d091f77ebcdb220d555ab78b98
Maarten Balliauw <maarten@balliauw.be>
2013-05-03 22:37:58 +0200

    Updated build.bat versioning
    Updated build.bat versioning

--- Build.bat
+++ Build.bat
@@ -6,8 +6,11 @@ if "%config%" == "" (
 )
 
 set version=4.0.0
-if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
+REM if not "%PackageVersion%" == "" (
+REM    set version=%PackageVersion%
+REM )
+if "%PackageVersion%" == "" (
+    set PackageVersion=%version%
 )
 
-powershell -Command "& { Import-Module .\build\psake.psm1; $psake.use_exit_on_error = $true; Invoke-psake .\build\build.ps1 -framework 4.0x64 -properties @{\"version\"=\"%version%\";\"configuration\"=\"%config%"\"} }"
+powershell -Command "& { Import-Module .\build\psake.psm1; $psake.use_exit_on_error = $true; Invoke-psake .\build\build.ps1 -framework 4.0x64 -properties @{\"version\"=\"%version%\";\"configuration\"=\"%config%"\";\"packageVersion\"=\"%PackageVersion%"\"} }"
