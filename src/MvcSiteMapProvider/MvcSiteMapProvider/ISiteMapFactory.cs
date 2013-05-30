6d32c2fcb49bc9062a7b07a0dfd993c76e568015
Cornelius Kopp <cornelius.kopp@axonn.co.uk>
2013-05-30 12:38:43 +0100

    Fixes typo in ISiteMapFactory xmldoc, was referencing the wrong type to create

--- src/MvcSiteMapProvider/MvcSiteMapProvider/ISiteMapFactory.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/ISiteMapFactory.cs
@@ -4,7 +4,7 @@ using MvcSiteMapProvider.Builder;
 namespace MvcSiteMapProvider
 {
     /// <summary>
-    /// Contract for an abstract factory that creates new instances of <see cref="T:MvcSiteMapProvider.ISiteMapFactory"/> at runtime.
+    /// Contract for an abstract factory that creates new instances of <see cref="T:MvcSiteMapProvider.ISiteMap"/> at runtime.
     /// </summary>
     public interface ISiteMapFactory
     {
