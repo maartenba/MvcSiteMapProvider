32ffb1def92d631c30524d359cd8c9082725dbc7
Shad Storhaug <shad@shadstorhaug.com>
2013-06-01 06:02:36 +0700

    Added unit/integration test project to the solution.

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitleAttribute("MvcSiteMapProvider.Tests")]
+[assembly: AssemblyDescriptionAttribute("An unit and integration test project for MvcSiteMapProvider.")]
+[assembly: AssemblyProductAttribute("MvcSiteMapProvider.Tests")]
