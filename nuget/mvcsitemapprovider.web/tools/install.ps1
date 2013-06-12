fe0d76795c1684317dbc7f609678617ea60239f6
Shad Storhaug <shad@shadstorhaug.com>
2013-06-13 04:07:01 +0700

    Added fix for #124 to the build - need to add section to web.config for MVC4 only.

--- nuget/mvcsitemapprovider.web/tools/install.ps1
+++ nuget/mvcsitemapprovider.web/tools/install.ps1
@@ -15,9 +15,75 @@ function InferPreferredViewEngine() {
 	return "aspx"
 }
 
+function Install-MVC4-Config-Sections() {
+	$xml = New-Object xml
+
+	# find the Web.config file
+	$config = $project.ProjectItems | where {$_.Name -eq "Web.config"}
+
+	# find its path on the file system
+	$localPath = $config.Properties | where {$_.Name -eq "LocalPath"}
+
+	# load Web.config as XML
+	$xml.Load($localPath.Value)
+
+	# select the node
+	#$node = $xml.SelectSingleNode("configuration/appSettings/add[@key='MvcSiteMapProvider_UseExternalDIContainer']")
+	$ws = $xml.SelectSingleNode("configuration/system.webServer")
+	if ($ws -eq $null) {
+		$ws = $xml.CreateElement("system.webServer")
+		$xml.AppendChild($ws)
+	}
+	
+	$modules = $xml.SelectSingleNode("configuration/system.webServer/modules")
+	if ($modules -eq $null) {
+		$modules = $xml.CreateElement("modules")
+		$ws.AppendChild($modules)
+	}
+	
+	$remove = $xml.SelectSingleNode("configuration/system.webServer/modules/remove[@name='UrlRoutingModule-4.0']")
+	if ($remove -eq $null) {
+		$remove = $xml.CreateElement("remove")
+		
+		$name = $xml.CreateAttribute("name")
+		$name.Value = "UrlRoutingModule-4.0"
+		$remove.Attributes.Append($name)
+		
+		$modules.AppendChild($remove)
+	}
+	
+	$add = $xml.SelectSingleNode("configuration/system.webServer/modules/add[@name='UrlRoutingModule-4.0']")
+	if ($add -eq $null) {
+		$add = $xml.CreateElement("add")
+		
+		$name = $xml.CreateAttribute("name")
+		$name.Value = "UrlRoutingModule-4.0"
+		$add.Attributes.Append($name)
+		
+		$type = $xml.CreateAttribute("type")
+		$type.Value = "System.Web.Routing.UrlRoutingModule"
+		$add.Attributes.Append($type)
+		
+		$modules.AppendChild($add)
+	}
+	
+	# save the Web.config file
+	$xml.Save($localPath.Value)
+}
+
+
+
 if ([string](InferPreferredViewEngine) -eq "aspx") { 
 	(Get-Project).ProjectItems | ?{ $_.Name -eq "Views" } | %{ $_.ProjectItems | ?{ $_.Name -eq "Shared" } } | %{ $_.ProjectItems | ?{ $_.Name -eq "DisplayTemplates" } } | %{ $_.ProjectItems | ?{ $_.Name -eq "MenuHelperModel.cshtml" -or  $_.Name -eq "SiteMapHelperModel.cshtml" -or  $_.Name -eq "SiteMapNodeModel.cshtml" -or  $_.Name -eq "SiteMapNodeModelList.cshtml" -or  $_.Name -eq "SiteMapPathHelperModel.cshtml" -or  $_.Name -eq "SiteMapTitleHelperModel.cshtml" -or  $_.Name -eq "CanonicalHelperModel.cshtml" -or  $_.Name -eq "MetaRobotsHelperModel.cshtml" } } | %{ $_.Delete() }
 } else {
 	(Get-Project).ProjectItems | ?{ $_.Name -eq "Views" } | %{ $_.ProjectItems | ?{ $_.Name -eq "Shared" } } | %{ $_.ProjectItems | ?{ $_.Name -eq "DisplayTemplates" } } | %{ $_.ProjectItems | ?{ $_.Name -eq "MenuHelperModel.ascx" -or  $_.Name -eq "SiteMapHelperModel.ascx" -or  $_.Name -eq "SiteMapNodeModel.ascx" -or  $_.Name -eq "SiteMapNodeModelList.ascx" -or  $_.Name -eq "SiteMapPathHelperModel.ascx" -or  $_.Name -eq "SiteMapTitleHelperModel.ascx" -or  $_.Name -eq "CanonicalHelperModel.ascx" -or  $_.Name -eq "MetaRobotsHelperModel.ascx" } } | %{ $_.Delete() }
 }
- 
+
+# If MVC 4, install web.config section to fix 404 not found on sitemap.xml (#124)
+
+if ($project.Object.References.Find("System.Web.Mvc").Version -eq "4.0.0.0")
+{
+	Write-Output("Detected MVC 4")
+	
+	Install-MVC4-Config-Sections
+}
