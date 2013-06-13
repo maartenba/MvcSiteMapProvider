f96e6658c5522cd9b1c14f79d8cfd61a231418dc
Shad Storhaug <shad@shadstorhaug.com>
2013-06-14 04:35:30 +0700

    Copied aspx vs cshtml code verbatim from MvcMailer to see if it will help - aspx files still not being preserved in MVC2 project.

--- nuget/mvcsitemapprovider.web/tools/install.ps1
+++ nuget/mvcsitemapprovider.web/tools/install.ps1
@@ -1,5 +1,6 @@
 param($rootPath, $toolsPath, $package, $project)
 
+### Copied from MvcScaffolding
 function CountSolutionFilesByExtension($extension) {
 	$files = (Get-Project).DTE.Solution `
 		| ?{ $_.FileName } `
@@ -10,10 +11,13 @@ function CountSolutionFilesByExtension($extension) {
 
 function InferPreferredViewEngine() {
 	# Assume you want Razor except if you already have some ASPX views and no Razor ones
-	if ((CountSolutionFilesByExtension "aspx") -eq 0) { return "razor" }
-	if (((CountSolutionFilesByExtension "cshtml") -gt 0) -or ((CountSolutionFilesByExtension "vbhtml") -gt 0)) { return "razor" }
+	if ((CountSolutionFilesByExtension aspx) -eq 0) { return "razor" }
+	if (((CountSolutionFilesByExtension cshtml) -gt 0) -or ((CountSolutionFilesByExtension vbhtml) -gt 0)) { return "razor" }
 	return "aspx"
 }
+
+# Infer which view engine you're using based on the files in your project
+### End copied
 
 function Install-MVC4-Config-Sections() {
 	$xml = New-Object xml
@@ -72,7 +76,7 @@ function Install-MVC4-Config-Sections() {
 
 
 
-if ([string](InferPreferredViewEngine) -eq "aspx") { 
+if ([string](InferPreferredViewEngine) -eq 'aspx') { 
 	(Get-Project).ProjectItems | ?{ $_.Name -eq "Views" } | %{ $_.ProjectItems | ?{ $_.Name -eq "Shared" } } | %{ $_.ProjectItems | ?{ $_.Name -eq "DisplayTemplates" } } | %{ $_.ProjectItems | ?{ $_.Name -eq "MenuHelperModel.cshtml" -or  $_.Name -eq "SiteMapHelperModel.cshtml" -or  $_.Name -eq "SiteMapNodeModel.cshtml" -or  $_.Name -eq "SiteMapNodeModelList.cshtml" -or  $_.Name -eq "SiteMapPathHelperModel.cshtml" -or  $_.Name -eq "SiteMapTitleHelperModel.cshtml" -or  $_.Name -eq "CanonicalHelperModel.cshtml" -or  $_.Name -eq "MetaRobotsHelperModel.cshtml" } } | %{ $_.Delete() }
 } else {
 	(Get-Project).ProjectItems | ?{ $_.Name -eq "Views" } | %{ $_.ProjectItems | ?{ $_.Name -eq "Shared" } } | %{ $_.ProjectItems | ?{ $_.Name -eq "DisplayTemplates" } } | %{ $_.ProjectItems | ?{ $_.Name -eq "MenuHelperModel.ascx" -or  $_.Name -eq "SiteMapHelperModel.ascx" -or  $_.Name -eq "SiteMapNodeModel.ascx" -or  $_.Name -eq "SiteMapNodeModelList.ascx" -or  $_.Name -eq "SiteMapPathHelperModel.ascx" -or  $_.Name -eq "SiteMapTitleHelperModel.ascx" -or  $_.Name -eq "CanonicalHelperModel.ascx" -or  $_.Name -eq "MetaRobotsHelperModel.ascx" } } | %{ $_.Delete() }
