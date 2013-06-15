3a2d0ffe4727457ab1f69c6a39715b09c6ba8508
Shad Storhaug <shad@shadstorhaug.com>
2013-06-15 17:35:15 +0700

    Added preliminary (non-functioning) Ninject configuration.

--- src/MvcSiteMapProvider/CodeAsConfiguration/Ninject/DI/Ninject/Modules/MvcModule.cs
+++ src/MvcSiteMapProvider/CodeAsConfiguration/Ninject/DI/Ninject/Modules/MvcModule.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using MvcSiteMapProvider;
 using MvcSiteMapProvider.Web.Mvc;
 using Ninject;
 using Ninject.Modules;
+using Ninject.Extensions.Conventions;
 
 namespace DI.Ninject.Modules
 {
@@ -11,8 +16,21 @@ namespace DI.Ninject.Modules
     {
         public override void Load()
         {
-            // TODO: Add Ninject Mvc implementation here
-            throw new NotImplementedException();
+            var currentAssembly = typeof(MvcModule).Assembly;
+
+            // Fix for controllers - need to ensure they are transient or http scoped or
+            // there will be problems when using dependency injection.
+            //http://code-inside.de/blog/2011/01/18/fix-a-single-instance-of-controller-foocontroller-cannot-be-used-to-handle-multiple-requests-mvc3/
+            this.Kernel.Bind(scanner => scanner.From(new Assembly[] { currentAssembly, typeof(SiteMaps).Assembly })
+                   .Select(IsControllerType)
+                   .BindDefaultInterface()
+                   .Configure(binding => binding.InTransientScope())
+                   );
+        }
+
+        private bool IsControllerType(Type type)
+        {
+            return type.IsClass && type.GetInterfaces().Any(intface => intface.GetType().Equals(typeof(IController)));
         }
     }
 }
--- src/MvcSiteMapProvider/CodeAsConfiguration/Ninject/DI/Ninject/Modules/MvcSiteMapProviderModule.cs
+++ src/MvcSiteMapProvider/CodeAsConfiguration/Ninject/DI/Ninject/Modules/MvcSiteMapProviderModule.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Web.Mvc;
 using System.Web.Hosting;
+using System.Runtime.Caching;
 using MvcSiteMapProvider;
 using MvcSiteMapProvider.Web.Mvc;
 using MvcSiteMapProvider.Web.Compilation;
@@ -16,16 +19,123 @@ using MvcSiteMapProvider.Xml;
 using MvcSiteMapProvider.Globalization;
 using Ninject;
 using Ninject.Modules;
+using Ninject.Extensions.Conventions;
+
 
 namespace DI.Ninject.Modules
 {
     public class MvcSiteMapProviderModule
         : NinjectModule
     {
+
+        // NOTE: This code is preliminary and still is not functional!!
+
         public override void Load()
         {
-            // TODO: Add Ninject MvcSiteMapProvider registration implementation here
-            throw new NotImplementedException();
+            var currentAssembly = typeof(MvcSiteMapProviderModule).Assembly;
+            string absoluteFileName = HostingEnvironment.MapPath("~/Mvc.sitemap");
+            TimeSpan absoluteCacheExpiration = TimeSpan.FromMinutes(5);
+            string[] includeAssembliesForScan = new string[] { "$AssemblyName$" };
+
+            this.Kernel.Bind(scanner => scanner.From(new Assembly[] { currentAssembly, typeof(SiteMaps).Assembly })
+                   .Select(IsServiceType)
+#if !MVC2
+                   .Excluding<FilterProvider>()
+#endif
+                   .BindDefaultInterface()
+                   .Configure(binding => binding.InSingletonScope())
+                   );
+
+
+            this.Kernel.Bind<ControllerBuilder>().ToConstant(ControllerBuilder.Current);
+            this.Kernel.Bind<IControllerBuilder>().To<ControllerBuilderAdaptor>();
+            this.Kernel.Bind<IBuildManager>().To<BuildManagerAdaptor>();
+
+#if !MVC2
+            // Configure default filter provider with one that provides filters
+            // from the global filter collection.
+            this.Kernel.Bind<IFilterProvider>().To<FilterProvider>().InSingletonScope();
+#endif
+
+            // Configure Security
+            this.Kernel.Bind<IAclModule>().To<AuthorizeAttributeAclModule>().Named("authorizeAttribute");
+            this.Kernel.Bind<IAclModule>().To<XmlRolesAclModule>().Named("xmlRoles");
+            this.Kernel.Bind<IAclModule>().To<CompositeAclModule>()
+                .WithConstructorArgument("aclModules", 
+                    new IAclModule[] { 
+                        this.Kernel.Get<IAclModule>("authorizeAttribute"), 
+                        this.Kernel.Get<IAclModule>("xmlRoles") 
+                    });
+
+
+            // Setup cache
+#if NET35
+            this.Kernel.Bind<ISiteMapCache>().To<AspNetSiteMapCache>();
+            this.Kernel.Bind<ICacheDependency>().To<AspNetFileCacheDependency>().Named("cacheDependency1")
+                .WithConstructorArgument("fileName", absoluteFileName);
+#else
+            this.Kernel.Bind<ObjectCache>().ToConstant<ObjectCache>(MemoryCache.Default);
+            this.Kernel.Bind<ISiteMapCache>().To<RuntimeSiteMapCache>();
+            this.Kernel.Bind<ICacheDependency>().To<RuntimeFileCacheDependency>().Named("cacheDependency1")
+                .WithConstructorArgument("fileName", absoluteFileName);
+#endif
+            this.Kernel.Bind<ICacheDetails>().To<CacheDetails>().Named("cacheDetails1")
+                .WithConstructorArgument("absoluteCacheExpiration", absoluteCacheExpiration)
+                .WithConstructorArgument("slidingCacheExpiration", TimeSpan.MinValue)
+                .WithConstructorArgument("cacheDependency", this.Kernel.Get<ICacheDependency>("cacheDependency1"));
+
+
+            // Configure the visitors
+            this.Kernel.Bind<ISiteMapNodeVisitor>().To<UrlResolvingSiteMapNodeVisitor>();
+
+            // Register the sitemap builder
+            this.Kernel.Bind<IXmlSource>().To<FileXmlSource>().Named("xmlBuilderXmlSource")
+                .WithConstructorArgument("fileName", absoluteFileName);
+            this.Kernel.Bind<ISiteMapXmlReservedAttributeNameProvider>().To<SiteMapXmlReservedAttributeNameProvider>().Named("xmlBuilderReservedAttributeNameProvider")
+                .WithConstructorArgument("attributesToIgnore", new string[0]);
+
+            // Xml Builder
+            this.Kernel.Bind<ISiteMapBuilder>().To<XmlSiteMapBuilder>().Named("xmlSiteMapBuilder")
+                .WithConstructorArgument("reservedAttributeNameProvider", this.Kernel.Get<ISiteMapXmlReservedAttributeNameProvider>("xmlBuilderReservedAttributeNameProvider"))
+                .WithConstructorArgument("xmlSource", this.Kernel.Get<IXmlSource>("xmlBuilderXmlSource"));
+
+            
+
+            // Reflection Builder
+            this.Kernel.Bind<ISiteMapBuilder>().To<ReflectionSiteMapBuilder>().Named("reflectionSiteMapBuilder")
+                .WithConstructorArgument("includeAssemblies", includeAssembliesForScan)
+                .WithConstructorArgument("excludeAssemblies", new string[0]);
+
+            // Visiting Builder
+            this.Kernel.Bind<ISiteMapBuilder>().To<VisitingSiteMapBuilder>().Named("visitingSiteMapBuilder");
+
+
+            // Composite builder
+            this.Kernel.Bind<ISiteMapBuilder>().To<CompositeSiteMapBuilder>().Named("compositeSiteMapBuilder")
+                .WithConstructorArgument("siteMapBuilders", 
+                    new ISiteMapBuilder[] { 
+                        this.Kernel.Get<ISiteMapBuilder>("xmlSiteMapBuilder"),
+                        this.Kernel.Get<ISiteMapBuilder>("reflectionSiteMapBuilder"),
+                        this.Kernel.Get<ISiteMapBuilder>("visitingSiteMapBuilder")
+                    });
+
+
+            // Configure the builder sets
+            this.Kernel.Bind<ISiteMapBuilderSet>().To<SiteMapBuilderSet>().Named("siteMapBuilderSet1")
+                .WithConstructorArgument("instanceName", "default")
+                .WithConstructorArgument("siteMapBuilder", this.Kernel.Get<ISiteMapBuilder>("compositeSiteMapBuilder"))
+                .WithConstructorArgument("cacheDetails", this.Kernel.Get<ICacheDetails>("cacheDetails1"));
+
+            this.Kernel.Bind<ISiteMapBuilderSetStrategy>().To<SiteMapBuilderSetStrategy>()
+                .WithConstructorArgument("siteMapBuilderSets",
+                    new ISiteMapBuilderSet[] {
+                        this.Kernel.Get<ISiteMapBuilderSet>("siteMapBuilderSet1")
+                    });
+        }
+
+        private bool IsServiceType(Type type)
+        {
+            return type.IsClass && type.GetInterfaces().Any(intface => intface.Name == "I" + type.Name);
         }
     }
 }
