9bfc26411e5c4e3f5fc2fd774da68555d817d78e
Shad Storhaug <shad@shadstorhaug.com>
2013-08-04 15:12:25 +0700

    Fixes #89, added TrimEmptyGroupingNodesVisibilityProvider to remove non-clickable nodes that have no accessible children.

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/TrimEmptyGroupingNodesVisibilityProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcSiteMapProvider
+{
+    public class TrimEmptyGroupingNodesVisibilityProvider
+        : SiteMapNodeVisibilityProviderBase
+    {
+        public override bool IsVisible(ISiteMapNode node, IDictionary<string, object> sourceMetadata)
+        {
+            if (!node.HasChildNodes && !node.Clickable)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
