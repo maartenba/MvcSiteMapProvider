f017df8e4c0f058a64b5c2f902acbae9ad8d96ad
Maarten Balliauw <maarten@balliauw.be>
2013-09-10 15:20:19 +0200

    #224 Version Not Incrementing

--- build/runbuild.ps1
+++ build/runbuild.ps1
@@ -18,14 +18,20 @@ task Clean -description "This task cleans up the build directory" {
 }
 
 task Init -description "This tasks makes sure the build environment is correctly setup" {  
-	if ($env:BuildRunner -eq $null -or $env:BuildRunner -ne "MyGet") {
-		Generate-Assembly-Info `
-			-file "$source_directory\Shared\CommonAssemblyInfo.cs" `
-			-company "MvcSiteMapProvider" `
-			-version $version `
-			-packageVersion $packageVersion `
-			-copyright "Copyright © MvcSiteMapProvider 2009 - 2013"
+	if ($env:BuildRunner -ne $null -and $env:BuildRunner -eq "MyGet") {		
+		$version = $packageVersion
+		if ($version.Contains("-") -eq $true) {
+			$version = $version.SubString(0, $version.IndexOf("-"))
+		}
+		echo "Updated version to: $version"
 	}
+	
+	Generate-Assembly-Info `
+		-file "$source_directory\Shared\CommonAssemblyInfo.cs" `
+		-company "MvcSiteMapProvider" `
+		-version $version `
+		-packageVersion $packageVersion `
+		-copyright "Copyright © MvcSiteMapProvider 2009 - 2013"
 }
 
 task Compile -depends Clean, Init -description "This task compiles the solution" {
@@ -58,15 +64,13 @@ task NuGet -depends Compile -description "This tasks makes creates the NuGet pac
 }
 
 task Finalize -depends NuGet -description "This tasks finalizes the build" {  
-	if ($env:BuildRunner -eq $null -or $env:BuildRunner -ne "MyGet") {
-		#Change the assembly info file to be the same way it was before
-		Generate-Assembly-Info `
-			-file "$source_directory\Shared\CommonAssemblyInfo.cs" `
-			-company "MvcSiteMapProvider" `
-			-version $version `
-			-packageVersion $version `
-			-copyright "Copyright © MvcSiteMapProvider 2009 - 2013"
-	}
+	#Change the assembly info file to be the same way it was before
+	Generate-Assembly-Info `
+		-file "$source_directory\Shared\CommonAssemblyInfo.cs" `
+		-company "MvcSiteMapProvider" `
+		-version $version `
+		-packageVersion $version `
+		-copyright "Copyright © MvcSiteMapProvider 2009 - 2013"
 }
 
 function Transform-Nuspec ([string] $source, [string] $transform, [string] $destination) {
