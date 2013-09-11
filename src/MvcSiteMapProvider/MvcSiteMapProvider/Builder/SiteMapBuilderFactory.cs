74313f4b70da45ec6a674fa1d6a41b1d5c4b78bc
Shad Storhaug <shad@shadstorhaug.com>
2013-09-12 04:02:53 +0700

    Created SiteMapBuilderFactory to assist with DI containers that do not have a way to resolve single parameters of constructors.

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Builder/SiteMapBuilderFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MvcSiteMapProvider.Caching;
+using MvcSiteMapProvider.Visitor;
+
+namespace MvcSiteMapProvider.Builder
+{
+    public class SiteMapBuilderFactory
+    {
+        public SiteMapBuilderFactory(
+            ISiteMapNodeVisitor siteMapNodeVisitor,
+            ISiteMapHierarchyBuilder siteMapHierarchyBuilder,
+            ISiteMapCacheKeyGenerator siteMapCacheKeyGenerator,
+            ISiteMapNodeHelperFactory siteMapNodeHelperFactory
+            )
+        {
+            if (siteMapNodeVisitor == null)
+                throw new ArgumentNullException("siteMapNodeVisitor");
+            if (siteMapHierarchyBuilder == null)
+                throw new ArgumentNullException("siteMapHierarchyBuilder");
+            if (siteMapCacheKeyGenerator == null)
+                throw new ArgumentNullException("siteMapCacheKeyGenerator");
+            if (siteMapNodeHelperFactory == null)
+                throw new ArgumentNullException("siteMapNodeHelperFactory");
+
+            this.siteMapHierarchyBuilder = siteMapHierarchyBuilder;
+            this.siteMapCacheKeyGenerator = siteMapCacheKeyGenerator;
+            this.siteMapNodeHelperFactory = siteMapNodeHelperFactory;
+            this.siteMapNodeVisitor = siteMapNodeVisitor;
+        }
+        protected readonly ISiteMapHierarchyBuilder siteMapHierarchyBuilder;
+        protected readonly ISiteMapCacheKeyGenerator siteMapCacheKeyGenerator;
+        protected readonly ISiteMapNodeHelperFactory siteMapNodeHelperFactory;
+        protected readonly ISiteMapNodeVisitor siteMapNodeVisitor;
+
+        public virtual ISiteMapBuilder Create(ISiteMapNodeProvider siteMapNodeProvider)
+        {
+            return new SiteMapBuilder(
+                siteMapNodeProvider, 
+                this.siteMapNodeVisitor, 
+                this.siteMapHierarchyBuilder, 
+                this.siteMapCacheKeyGenerator, 
+                this.siteMapNodeHelperFactory);
+        }
+    }
+}
