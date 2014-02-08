8e21c5b8f14e0e4a2312092911d281acb588d1c4
Shad Storhaug <shad@shadstorhaug.com>
2014-02-09 00:52:56 +0700

    Added [ExcludeFromAutoRegistration] attribute to AttributeAssemblyProvider because of a report that it had been automatically registered when it shouldn't have - http://stackoverflow.com/questions/21606494/.

--- src/MvcSiteMapProvider/MvcSiteMapProvider/Reflection/AttributeAssemblyProvider.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Reflection/AttributeAssemblyProvider.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using MvcSiteMapProvider.DI;
 
 namespace MvcSiteMapProvider.Reflection
 {
+    [ExcludeFromAutoRegistration]
     public class AttributeAssemblyProvider
         : IAttributeAssemblyProvider
     {
