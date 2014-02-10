4c54e0692f6a3fd359d7b1df9015759818b0c6ee
Shad Storhaug <shad@shadstorhaug.com>
2014-02-10 20:38:33 +0700

    Updated URL to the new getting started page - the old one was throwing a javascript error because it was hosted on the GitHub wiki, which doesn't support IE 8 (which is what is in Visual Studio 2012 and 2013). New page hosted on GitHub pages.

--- nuget/mvcsitemapprovider/tools/install.ps1
+++ nuget/mvcsitemapprovider/tools/install.ps1
@@ -4,4 +4,4 @@ param($rootPath, $toolsPath, $package, $project)
 #Delete the dummy readme file
 (Get-Project).ProjectItems | ?{ $_.Name -eq "MvcSiteMapProvider_Temp_ReadMe.txt" } | %{ $_.Delete() }
 
-$DTE.ItemOperations.Navigate("https://github.com/maartenba/MvcSiteMapProvider/wiki/Getting-Started", $DTE.vsNavigateOptions.vsNavigateOptionsNewWindow)
+$DTE.ItemOperations.Navigate("http://maartenba.github.io/MvcSiteMapProvider/getting-started.html", $DTE.vsNavigateOptions.vsNavigateOptionsNewWindow)
