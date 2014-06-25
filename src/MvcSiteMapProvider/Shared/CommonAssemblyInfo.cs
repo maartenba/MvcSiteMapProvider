e10e634f625afa2e13d780641cccb8be0b9fbe7e
Ian Johnson <ipjohnson123@yahoo.com>
2014-06-24 21:28:10 -0600

    reverting back

--- src/MvcSiteMapProvider/Shared/CommonAssemblyInfo.cs
+++ src/MvcSiteMapProvider/Shared/CommonAssemblyInfo.cs
@@ -6,9 +6,8 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliantAttribute(false)]
 [assembly: ComVisibleAttribute(false)]
 [assembly: AssemblyCompanyAttribute("MvcSiteMapProvider")]
-[assembly: AssemblyCopyrightAttribute("Copyright © MvcSiteMapProvider 2009 - 2014")]
-[assembly: AssemblyVersionAttribute("4.6.3")]
-[assembly: AssemblyInformationalVersionAttribute("4.6.3")]
-[assembly: AssemblyFileVersionAttribute("4.6.3")]
+[assembly: AssemblyCopyrightAttribute("Copyright © MvcSiteMapProvider 2009 - 2013")]
+[assembly: AssemblyVersionAttribute("4.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("4.0.0")]
+[assembly: AssemblyFileVersionAttribute("4.0.0")]
 [assembly: AssemblyDelaySignAttribute(false)]
-
