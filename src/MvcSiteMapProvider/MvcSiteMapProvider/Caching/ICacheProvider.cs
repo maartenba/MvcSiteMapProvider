e0dfa5e61b368a2cac791ae50a75ae57b0b68d87
Shad Storhaug <shad@shadstorhaug.com>
2014-09-03 01:23:33 +0700

    Added reminder to refactor the event handler of the ICacheProvider interface in version 5.

--- src/MvcSiteMapProvider/MvcSiteMapProvider/Caching/ICacheProvider.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/Caching/ICacheProvider.cs
@@ -14,6 +14,10 @@ namespace MvcSiteMapProvider.Caching
         bool TryGetValue(string key, out LazyLock value);
         void Add(string key, LazyLock item, ICacheDetails cacheDetails);
         void Remove(string key);
+
+        // NOTE: In version 5, this should be changed to a ProviderItemRemovedEventArgs to pass the LazyLock
+        // instead of T back to the MicroCache to make it consistently deal with a LazyLock and the
+        // MicroCache consistently deal with a T.
         event EventHandler<MicroCacheItemRemovedEventArgs<T>> ItemRemoved;
     }
 }
