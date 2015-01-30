b0e31056a40a3c0c0c0fef3f9979b7bfe1cb5bfb
Shad Storhaug <shad@shadstorhaug.com>
2015-01-30 22:45:08 +0700

    Fixes #375, TrimEmptyGroupingNodesVisibilityProvider doesn't work with nested visibility providers.

--- src/MvcSiteMapProvider/MvcSiteMapProvider/TrimEmptyGroupingNodesVisibilityProvider.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider/TrimEmptyGroupingNodesVisibilityProvider.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MvcSiteMapProvider
 {
     /// <summary>
-    /// Removes non-clickable nodes that have no accessible children.
+    /// Removes non-clickable nodes that have no accessible and/or visible children.
     /// </summary>
     public class TrimEmptyGroupingNodesVisibilityProvider
         : SiteMapNodeVisibilityProviderBase
     {
         public override bool IsVisible(ISiteMapNode node, IDictionary<string, object> sourceMetadata)
         {
-            if (!node.HasChildNodes && !node.Clickable)
+            // Optimization - return quickly if clickable.
+            if (node.Clickable)
             {
-                return false;
+                return true;
             }
-            return true;
+
+            var childNodes = node.ChildNodes;
+            return childNodes == null || childNodes.Any(c => c.IsVisible(sourceMetadata));
         }
     }
 }
